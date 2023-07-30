2001a564b54f3c608ae8b3a5179d43e2646d68fb
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-07-30 21:10:58 +0200

    doc: Adds comments to bit

--- include/jank/bit/bit.hpp
+++ include/jank/bit/bit.hpp
@@ -1,9 +1,20 @@
 #ifndef JANK_BIT
 #define JANK_BIT
 
-#include <cstdint>
 #include <cassert>
+#include <cstdint>
+
+/**
+* NOTE: These are not tested on 32-bit architecture, and might(likely) not work as intended
+*/ 
+
 namespace jank::bit {
+/**
+* @brief Returns the number of trailing 0-bits in x, starting from the least significant bit
+* @param x A non-zero unsigned 16 bit number
+* @example ...01 -> 0 
+* @example ...10 -> 1
+*/
 inline int lsb(uint16_t x) {
 #if defined(__GNUC__)
     return __builtin_ctz(x);
@@ -12,6 +23,12 @@ inline int lsb(uint16_t x) {
 #endif
 }
 
+/**
+* @brief Returns the number of trailing 0-bits in x, starting from the least significant bit
+* @param x A non-zero unsigned 32 bit number
+* @example ...01 -> 0 
+* @example ...10 -> 1
+*/
 inline int lsb(uint32_t x) {
 #if defined(__GNUC__)
     return __builtin_ctzl(x);
@@ -20,6 +37,12 @@ inline int lsb(uint32_t x) {
 #endif
 }
 
+/**
+* @brief Returns the number of trailing 0-bits in x, starting from the least significant bit
+* @param x A non-zero unsigned 64 bit number
+* @example ...01 -> 0 
+* @example ...10 -> 1
+*/
 inline int lsb(uint64_t x) {
 #if defined(__GNUC__)
     return __builtin_ctzll(x);
@@ -28,6 +51,12 @@ inline int lsb(uint64_t x) {
 #endif
 }
 
+/**
+* @brief Returns the number of leading 0-bits in x, starting from the most significant bit
+* @param x A non-zero unsigned 16 bit number
+* @example 10... -> 0 
+* @example 01... -> 1
+*/
 inline int msb(uint16_t x) {
 #if defined(__GNUC__)
     assert(sizeof(unsigned int) == 4);
@@ -37,15 +66,32 @@ inline int msb(uint16_t x) {
 #endif
 }
 
+/**
+* @brief Returns the number of leading 0-bits in x, starting from the most significant bit
+* @param x A non-zero unsigned 32 bit number
+* @example 10... -> 0 
+* @example 01... -> 1
+*/
 inline int msb(uint32_t x) {
 #if defined(__GNUC__)
+#if __x86_64__ || __ppc64__
     assert(sizeof(unsigned long) == 8);
     return 63 ^ __builtin_clzl(x);
+#else
+    assert(sizeof(unsigned long) == 4);
+    return 31 ^ __builtin_clzl(x);
+#endif
 #else
 #error
 #endif
 }
 
+/**
+* @brief Returns the number of leading 0-bits in x, starting from the most significant bit
+* @param x A non-zero unsigned 64 bit number
+* @example 10... -> 0 
+* @example 01... -> 1
+*/
 inline int msb(uint64_t x) {
 #if defined(__GNUC__)
     assert(sizeof(unsigned long long) == 8);
@@ -55,6 +101,12 @@ inline int msb(uint64_t x) {
 #endif
 }
 
+/**
+* @brief Returns the number of 1-bits in x
+* @param x An unsigned 16 bit number
+* @example 100... -> 1
+* @example 110... -> 2
+*/
 inline int popcount(uint16_t x) {
 #if defined(__GNUC__)
     return __builtin_popcount(x);
@@ -63,6 +115,12 @@ inline int popcount(uint16_t x) {
 #endif
 }
 
+/**
+* @brief Returns the number of 1-bits in x
+* @param x An unsigned 32 bit number
+* @example 100... -> 1
+* @example 110... -> 2
+*/
 inline int popcount(uint32_t x) {
 #if defined(__GNUC__)
     return __builtin_popcountl(x);
@@ -71,6 +129,12 @@ inline int popcount(uint32_t x) {
 #endif
 }
 
+/**
+* @brief Returns the number of 1-bits in x
+* @param x An unsigned 64 bit number
+* @example 100... -> 1
+* @example 110... -> 2
+*/
 inline int popcount(uint64_t x) {
 #if defined(__GNUC__)
     return __builtin_popcountll(x);
